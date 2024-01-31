0a049e20c6f49472835951d69856b31644de29a8
Vlastimil Myslivecek <120052173+Vlastiss@users.noreply.github.com>
2024-01-31 18:26:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const capitalLetter = document.getElementById('capitalLetter');
 const specialChar = document.getElementById('specialChar');
 const number = document.getElementById('number');
 
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+const capitalCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const specialCharacters = '!£$%^&*#@,._/'
