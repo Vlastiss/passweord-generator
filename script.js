8ef3f29a918173eef3ab8362f190b75d660bc80f
Vlastimil Myslivecek <120052173+Vlastiss@users.noreply.github.com>
2024-01-31 18:20:26 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const charLength = document.getElementById('charLength');
+const capitalLetter = document.getElementById('capitalLetter');
+const specialChar = document.getElementById('specialChar');
+const number = document.getElementById('number');
+
