fd25f5fd70a8ee76a12566e0d2da8eeb30144be7
Vlastimil Myslivecek <120052173+Vlastiss@users.noreply.github.com>
2024-01-31 20:18:03 +0100

    final

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var capitalLetter = document.getElementById('capitalLetter');
 var specialChar = document.getElementById('specialChar');
 var number = document.getElementById('number');
 var passwordLengthDisplay = document.getElementById('passwordLengthDisplay');
+var passwordArea = document.getElementById('password');
 
 const numbers = '0123456789';
 const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
@@ -11,9 +12,13 @@ const specialCharacters = '!£$%^&*#@,._/+-*=';
 
 let generatedPassword = '';
 
+passwordLengthDisplay.innerHTML = charLengthInput.value;
+charLengthInput.addEventListener('input', function() {
+    passwordLengthDisplay.innerHTML = charLengthInput.value;
+});
+
 function generatePassword() {
     const length = parseInt(charLengthInput.value);
-    console.log(length); // Log the password length for debugging purposes
 
     generatedPassword = '';
 
@@ -22,18 +27,24 @@ function generatePassword() {
         return 'Invalid password length';
     }
 
+    const maxCapitalLength = capitalLetter.checked ? Math.floor(length / 2) : 0;
+    const maxSpecialLength = specialChar.checked ? Math.floor(length / 2) : 0;
+    const maxNumberLength = number.checked ? Math.floor(length / 2) : 0;
+
     for (let i = 0; i < length; i++) {
-        if (capitalLetter.checked) {
+        if (i < maxCapitalLength && capitalLetter.checked) {
             generatedPassword += capitalCaseLetters.charAt(Math.floor(Math.random() * capitalCaseLetters.length));
         }
-        if (specialChar.checked) {
+        if (i < maxSpecialLength && specialChar.checked) {
             generatedPassword += specialCharacters.charAt(Math.floor(Math.random() * specialCharacters.length));
         }
-        if (number.checked) {
+        if (i < maxNumberLength && number.checked) {
             generatedPassword += numbers.charAt(Math.floor(Math.random() * numbers.length));
         }
+        generatedPassword += lowerCaseLetters.charAt(Math.floor(Math.random() * lowerCaseLetters.length));
     }
 
-    console.log(generatedPassword);
+    generatedPassword = generatedPassword.slice(0, length);
+    passwordArea.innerHTML = generatedPassword;
     return generatedPassword;
 }
