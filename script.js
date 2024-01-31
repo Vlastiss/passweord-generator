344fa1335a4d1af0c169c20610f516a566bcb8d6
Vlastimil Myslivecek <120052173+Vlastiss@users.noreply.github.com>
2024-01-31 19:47:27 +0100

    update

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
-var charLength = document.getElementById('charLength');
+var charLengthInput = document.getElementById('charLength');
 var capitalLetter = document.getElementById('capitalLetter');
 var specialChar = document.getElementById('specialChar');
 var number = document.getElementById('number');
 var passwordLengthDisplay = document.getElementById('passwordLengthDisplay');
 
-const numbers = '0123456789'
-const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
-const capitalCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-const specialCharacters = '!£$%^&*#@,._/+-*='
+const numbers = '0123456789';
+const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+const capitalCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const specialCharacters = '!£$%^&*#@,._/+-*=';
 
-function generatePassword () {
+let generatedPassword = '';
 
-}
+function generatePassword() {
+    const length = parseInt(charLengthInput.value);
+    console.log(length); // Log the password length for debugging purposes
 
-capitalLetter.addEventListener('click', function () {
-    if (capitalLetter.checked) {
-        console.log(capitalLetter.value)
+    generatedPassword = '';
+
+    if (isNaN(length) || length <= 0) {
+        console.log('Invalid password length');
+        return 'Invalid password length';
     }
-} )
 
+    for (let i = 0; i < length; i++) {
+        if (capitalLetter.checked) {
+            generatedPassword += capitalCaseLetters.charAt(Math.floor(Math.random() * capitalCaseLetters.length));
+        }
+        if (specialChar.checked) {
+            generatedPassword += specialCharacters.charAt(Math.floor(Math.random() * specialCharacters.length));
+        }
+        if (number.checked) {
+            generatedPassword += numbers.charAt(Math.floor(Math.random() * numbers.length));
+        }
+    }
+
+    console.log(generatedPassword);
+    return generatedPassword;
+}
